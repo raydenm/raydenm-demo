815a8634880934f3c42a982a530bb45de1b4b49e
raydenm <raydenm928@gmail.com>
2024-03-28 10:36:13 +0800

    feat: change page

--- app/page.tsx
+++ app/page.tsx
@@ -1,58 +1,41 @@
-import { Metadata } from "next"
-import { Button } from "components/ui/button"
-import { LP_GRID_ITEMS } from "lp-items"
+"use client"
 
-export const metadata: Metadata = {
-  title: "Next.js Enterprise Boilerplate",
-  twitter: {
-    card: "summary_large_image",
-  },
-  openGraph: {
-    url: "https://next-enterprise.vercel.app/",
-    images: [
-      {
-        width: 1200,
-        height: 630,
-        url: "https://raw.githubusercontent.com/Blazity/next-enterprise/main/.github/assets/project-logo.png",
-      },
-    ],
-  },
-}
+import { useEffect, useState } from "react"
+import { getPeople } from "db/people"
+import type { Peoples } from "types/peopele"
+import AddPeople from "./components/people/add-people"
+import PeopleTabel from "./components/people/people-table"
 
-export default function Web() {
+const People = () => {
+  const [tableData, setTableData] = useState<Peoples[]>([])
+  const getData = async () => {
+    const data = await getPeople()
+    setTableData(data || [])
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
   return (
-    <>
-      <section className="bg-white dark:bg-gray-900">
-        <div className="mx-auto grid max-w-screen-xl px-4 py-8 text-center lg:py-16">
-          <div className="mx-auto place-self-center">
-            <h1 className="mb-4 max-w-2xl text-4xl font-extrabold leading-none tracking-tight dark:text-white md:text-5xl xl:text-6xl">
-              Next.js Enterprise Boilerplate
-            </h1>
-            <p className="mb-6 max-w-2xl font-light text-gray-500 dark:text-gray-400 md:text-lg lg:mb-8 lg:text-xl">
-              Jumpstart your enterprise project with our feature-packed, high-performance Next.js boilerplate!
-              Experience rapid UI development, AI-powered code reviews, and an extensive suite of tools for a smooth and
-              enjoyable development process.
-            </p>
-            <Button className="mr-3">Get started</Button>
-            <Button>Deploy Now</Button>
-          </div>
+    <section className="bg-white dark:bg-gray-900">
+      <div className="mx-auto grid max-w-screen-xl px-4 py-8 text-center lg:py-16">
+        <div className="mx-auto place-self-center">
+          <h1 className="mb-4 w-full max-w-2xl text-center text-4xl font-extrabold leading-none tracking-tight dark:text-white md:text-5xl xl:text-6xl">
+            Vercel Postgres
+          </h1>
+          <p className="mb-6 max-w-2xl font-light text-gray-500 dark:text-gray-400 md:text-lg lg:mb-8 lg:text-xl">
+            Vercel Postgres is a serverless SQL database designed to integrate with Vercel Functions and your frontend
+            framework.
+          </p>
         </div>
-      </section>
-      <section className="bg-white dark:bg-gray-900">
-        <div className="mx-auto max-w-screen-xl px-4 py-8 sm:py-16 lg:px-6">
-          <div className="justify-center space-y-8 md:grid md:grid-cols-2 md:gap-12 md:space-y-0 lg:grid-cols-3">
-            {LP_GRID_ITEMS.map((singleItem) => (
-              <div key={singleItem.title} className="flex flex-col items-center justify-center text-center">
-                <div className="bg-primary-100 dark:bg-primary-900 mb-4 flex size-10 items-center justify-center rounded-full p-1.5 text-blue-700 lg:size-12">
-                  {singleItem.icon}
-                </div>
-                <h3 className="mb-2 text-xl font-bold dark:text-white">{singleItem.title}</h3>
-                <p className="text-gray-500 dark:text-gray-400">{singleItem.description}</p>
-              </div>
-            ))}
-          </div>
-        </div>
-      </section>
-    </>
+      </div>
+      <div className="mx-auto max-w-screen-xl">
+        <AddPeople getData={getData}></AddPeople>
+        <PeopleTabel data={tableData} getData={getData}></PeopleTabel>
+      </div>
+    </section>
   )
 }
+
+export default People
